2b0db30b73a4cf55ea62351a5a18361fb2d0e703
Zvonimir X <xdwarrior@gmail.com>
2011-10-29 21:20:15 +0200

    3rd snap

---
+++ libs/base.h
@@ -0,0 +1,32 @@
+#ifndef BASE_H_INCLUDED
+#define BASE_H_INCLUDED
+
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <deque>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+typedef pair<int, int> par;
+typedef unsigned long long int ullint;
+typedef long long int llint;
+typedef unsigned int uint;
+typedef map< string, string > O_ARRAY;
+
+const int MAXN = 100001, MAXLEN = 255;
+const string _es = string();
+
+O_ARRAY Options, Consts;
+
+#endif // BASE_H_INCLUDED
